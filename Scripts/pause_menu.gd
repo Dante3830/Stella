0605b566a6b5850a04d62507a91db9788b563f81
Dante Rizzi <soydanterizzi@gmail.com>
2024-12-13 00:08:23 -0300

    s

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -30,10 +30,35 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	elif anim_name == "open":
 		$BoxContainer.visible = true
 
-func _on_options_button_pressed() -> void:
+# MAIN
+func _on_options_button_pressed():
 	$BoxContainer/MainContainer.visible = false
 	$BoxContainer/OptionsContainer.visible = true
 
-func _on_back_button_pressed() -> void:
+# OPTIONS
+func _on_game_button_pressed():
+	$BoxContainer/OptionsContainer.visible = false
+	$BoxContainer/GameOptContainer.visible = true
+
+func _on_sound_button_pressed():
+	$BoxContainer/SoundOptContainer.visible = true
+	$BoxContainer/OptionsContainer.visible = false
+
+func _on_back_button_pressed():
 	$BoxContainer/MainContainer.visible = true
 	$BoxContainer/OptionsContainer.visible = false
+
+# GAME
+func _on_game_opt_back_button_pressed():
+	$BoxContainer/OptionsContainer.visible = true
+	$BoxContainer/GameOptContainer.visible = false
+
+# SOUND
+func _on_sound_opt_back_button_pressed():
+	$BoxContainer/SoundOptContainer.visible = false
+	$BoxContainer/GameOptContainer.visible = true
+
+# GRAPHICS
+
+
+# CONTROLS
